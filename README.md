00f689575a5efb4e77ad0ca169a7ac4316c50f3b
Jonathan Perret <j-github@jonathanperret.net>
2021-02-26 17:48:33 +0100

    Initial implementation and README

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # mrc-client
-An Oculus Mixed Reality Capture client
+
+This tool will connect to an Oculus Quest VR headset running a VR app
+supporting the Oculus MRC protocol (as used by the official Oculus OBS
+plugin,
+https://github.com/facebookincubator/obs-plugins/tree/master/oculus-mrc),
+and display the video being streamed by the headset.
+
+Only the "background" part of the video stream is shown, in which actually
+the whole scene is rendered. In normal (mixed reality) usage, a real camera's
+chroma-keyed (i.e. green screen removed) image would be overlaid on that
+video, and then the "foreground" part of the video would be overlaid on top
+to show the objects that are closer to the camera than the player.
+
+I made this script to test the MRC output from my Quest games, without having
+to fire up OBS and also because the Oculus MRC plugin for OBS doesn't support
+macOS.
+
+## Requirements
+
+You need a reasonably recent NodeJS.
+
+You also need `ffplay` (which generally comes with `ffmpeg`, i.e.
+`brew install ffmpeg` or `sudo apt install ffmpeg`).
+
+This script was tested on macOS, it will probably work on Linux. Windows
+might need some changes.
+
+## Usage
+
+```
+npx https://github.com/jonathanperret/mrc-client x.x.x.x
+```
+
+Where `x.x.x.x` is to be replaced with the IP address of your Quest running
+an MRC-enabled VR app.
+
+If the tool can't connect to the MRC stream, it will keep trying, so you
+can launch it before going into your headset to launch the app.
---
+++ index.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+
+const { Socket } = require("net");
+const { spawn } = require('child_process');
+const { start } = require("repl");
+
+const serverAddress = process.argv[2];
+const PORT = 28734;
+
+const PayloadType = {
+    VIDEO_DIMENSION: 10,
+    VIDEO_DATA: 11,
+    AUDIO_SAMPLERATE: 12,
+    AUDIO_DATA: 13,
+};
+const FrameOffsets = {
+  PAYLOAD_TYPE: 8,
+  PAYLOAD_LENGTH: 12,
+  PAYLOAD_START: 16,
+}
+
+function startStreaming(done) {
+    let ffplay;
+    let shouldExit = false;
+
+    function startFFPlay() {
+        ffplay = spawn('ffplay', [
+            '-i', 'pipe:0',
+            '-nostats',
+            '-loglevel', 'error',
+            '-probesize', '32',
+            '-framerate', '100',
+            '-vf', 'crop=in_w/2:in_h:0:0,vflip'
+        ], {
+            stdio: ['pipe', 'inherit', 'inherit']
+        });
+
+        ffplay.on('exit', (code, signal) => {
+            console.log(`ffplay exited with ${code !== null ? code : signal}.`);
+            ffplay = null;
+            shouldExit = true;
+            socket.destroy();
+        });
+    }
+
+    const socket = new Socket();
+
+    let buf = Buffer.alloc(0);
+    socket.on("data", (data) => {
+        buf = Buffer.concat([buf, data]);
+        while (buf.length > FrameOffsets.PAYLOAD_START) {
+            const payloadLength = buf.readUInt32LE(FrameOffsets.PAYLOAD_LENGTH);
+            const packetSize = FrameOffsets.PAYLOAD_START + payloadLength;
+            if (buf.length < packetSize) return;
+            const type = buf.readUInt32LE(FrameOffsets.PAYLOAD_TYPE);
+            const payload = buf.slice(FrameOffsets.PAYLOAD_START, packetSize);
+            // console.log(type, payloadLength, payload);
+            if (type == PayloadType.VIDEO_DATA) {
+                ffplay.stdin.write(payload);
+            }
+            buf = buf.slice(packetSize);
+        }
+    });
+
+    socket.on('close', () => {
+        console.log('Connection closed.');
+        if (ffplay) {
+            ffplay.stdin.destroy();
+            ffplay.kill();
+        }
+        done(shouldExit);
+    });
+
+    socket.on('error', (err) => {
+        console.error('Socket error:', err.message);
+    });
+
+    console.log(`Connecting to ${serverAddress}:${PORT}`);
+
+    socket.connect(PORT, serverAddress, () => {
+        console.log('Connection established.');
+        shouldExit = true;
+        startFFPlay();
+    });
+}
+
+function startStreamingPromise() {
+    return new Promise((resolve) => {
+        startStreaming(resolve);
+    });
+}
+
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+    while (!await startStreamingPromise()) {
+        await delay(1000);
+    }
+}
+
+main();
