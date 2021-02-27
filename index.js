6ba680bb8fa41b6eeaeda81ce330ef4a181842c9
Fabio <fabio914@gmail.com>
2021-02-27 02:48:03 +0000

    Reading video dimensions

--- index.js
+++ index.js
@@ -21,15 +21,16 @@ const FrameOffsets = {
 function startStreaming(done) {
     let ffplay;
     let shouldExit = false;
+    let shouldStart = false;
 
-    function startFFPlay() {
+    function startFFPlay(width, height) {
         ffplay = spawn('ffplay', [
             '-i', 'pipe:0',
             '-nostats',
             '-loglevel', 'error',
             '-probesize', '32',
             '-framerate', '100',
-            '-vf', 'crop=in_w/2:in_h:0:0,vflip,scale=2*in_w:in_h'
+            '-vf', 'crop=in_w/2:in_h:0:0,vflip,scale=' + width + ':' + height + ''
         ], {
             stdio: ['pipe', 'inherit', 'inherit']
         });
@@ -54,8 +55,13 @@ function startStreaming(done) {
             const type = buf.readUInt32LE(FrameOffsets.PAYLOAD_TYPE);
             const payload = buf.slice(FrameOffsets.PAYLOAD_START, packetSize);
             // console.log(type, payloadLength, payload);
-            if (type == PayloadType.VIDEO_DATA) {
+            if (type == PayloadType.VIDEO_DATA && shouldStart) {
                 ffplay.stdin.write(payload);
+            } else if (type == PayloadType.VIDEO_DIMENSION && !shouldStart) {
+                const width = payload.readInt32LE(0);
+                const height = payload.readInt32LE(4);
+                startFFPlay(width/2.0, height);
+                shouldStart = true;
             }
             buf = buf.slice(packetSize);
         }
@@ -79,7 +85,6 @@ function startStreaming(done) {
     socket.connect(PORT, serverAddress, () => {
         console.log('Connection established.');
         shouldExit = true;
-        startFFPlay();
     });
 }
 
